83af763c4e43795de47ebcf7ddfcda25f2975c26
richarq-fisi <temporal98x@gmail.com>
2024-10-18 17:22:11 -0500

    without prettier

---
+++ src/app/(shop)/product/[id]/page.tsx
@@ -0,0 +1,3 @@
+export default function () {
+  return <div>Product Page</div>;
+}
---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,7 @@
+export default function () {
+  return (
+    <div className="text-white">
+      <h1>New Account</h1>
+    </div>
+  );
+}
